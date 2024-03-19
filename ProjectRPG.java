bdb2fdbd098578884f001dd1fea40cfddcfc5ff9
Saikuron089 <163764748+Saikuron089@users.noreply.github.com>
2024-03-19 17:58:25 +0100

    Add files via upload

--- ProjectRPG.java
+++ ProjectRPG.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class ProjectRPG {
 
-	FileReader f;
-	BufferedReader fIN;
+	static FileReader f;
+	static BufferedReader fIN;
 
 	// Funzione che pulisce lo schermo (system("cls") su java)
 	public static void clearScreen() {
@@ -13,31 +13,50 @@ public class ProjectRPG {
 		System.out.flush();
 	}
 
-	public static void MenuPrincipale(String[][] menu, int key, int posY) {
+	public static void MenuPrincipale(String[][] menu) throws InterruptedException {
 		Scanner scanner = new Scanner(System.in);
-		
-		posY = 1; // Il cursore si trova su "1) Gioca"
+
+		int currentLine = 0;
 
 		System.out.println("Usa W e S per sportarti su e giu'.");
 
 		Thread.sleep(3000); // Programma in pausa per 3 secondi
 
-		do{
+		while (true) {
 			clearScreen(); // Pulisce lo schermo
 
 			// Stampa il menu principale, il quale e' gestito con una matrice
 			for (int i = 0; i < menu.length; i++) {
-				for (int j = 0; j < menu[i].length; j++) {
-					System.out.print(menu[i][j]);
+				if (i == currentLine) {
+					System.out.println("--> " + menu[i][0]);
+				} else {
+					System.out.println("     " + menu[i][0]);
 				}
-				System.out.println();
 			}
-			key = System.in.read();
 
-			if(key==(char)'s'){
-				
+			// toLoweCase() serve per traformare tutto in minuscolo, per evitare errori
+			String scelta = scanner.nextLine().toLowerCase();
+
+			if (scelta.equals("w")) {
+				muoviFrecciaSu(currentLine);
+			} else if (scelta.equals("s")) {
+				muoviFrecciaGiu(currentLine, menu);
+			} else if (scelta.equals("")) {
+				// L'utente ha premuto INVIO
 			}
-		}while();
+		}
+	}
+
+	private static void muoviFrecciaSu(int currentLine) {
+		if (currentLine > 0) {
+			currentLine--;
+		}
+	}
+
+	private static void muoviFrecciaGiu(int currentLine, String[][] menu) {
+		if (currentLine < menu.length - 1) {
+			currentLine++;
+		}
 	}
 
 	// Funzione che mostra il layout del negozio
@@ -65,9 +84,7 @@ public class ProjectRPG {
 		System.out.println("");
 	}
 
-	// Funzione che evoca un peronaggio in modo casuale da fare
-
-	public String getRandomPlayer() {
+	public static String getRandomPlayer() {
 
 		// apre il file "personaggi_evocabili.txt"
 		try {
@@ -85,14 +102,10 @@ public class ProjectRPG {
 			String line = fIN.readLine();
 
 			while (line != null) {
-
 				v.addElement(line);
-
 				line = fIN.readLine();
 			}
-
 			f.close();
-
 		} catch (Exception e) {
 			System.out.println("Errore: " + e);
 		}
@@ -102,7 +115,6 @@ public class ProjectRPG {
 
 		// salva il personaggio sul file personaggi_ottenuti.txt
 		try {
-
 			// apro il file per la scrittura
 			FileWriter f1 = new FileWriter("personaggi_ottenuti.txt", true);
 			PrintWriter fOUT = new PrintWriter(f1);
@@ -121,23 +133,125 @@ public class ProjectRPG {
 
 			// chiudo il file
 			f1.close();
-
 		} catch (Exception e) {
 			System.out.println("Errore: " + e);
 		}
+
 		// ritorna la riga del personaggio in base al numero random
-		return "Hai trovato:  " + v.elementAt(scelta);
+		return "E' stato trovato il personaggio: " + v.elementAt(scelta);
 	}
 
-	public void Salva() {
+	public static void controlloStelle(String s, int cont) {
+		for (int i = 0; i < 10; i++) {
+			if (s.charAt(i) != '*' && cont < 10) { // per contare quante stelle ci sono già
+				cont++;
+			}
+		}
 
+		if (cont >= 10) {
+			return "";
+		}
 	}
 
-	public void Carica() {
-
+	public static void aggiungiStella(int cont, String s) {
+		if (cont < 10) {
+			s = '*' + s; // se le stelle sono meno di 10, ne aggiungo una
+			System.out.println("Il personaggio evocato aumenta di stelle!");
+		}
 	}
 
-	public static void main(String args[]) {
+	public Vector<String> mostraMissioni() {
+
+		// leggo tutte le missioni
+
+		Vector<String> v = new Vector<String>();
+
+		try {
+			FileReader f = new FileReader("missioni.txt");
+			BufferedReader fIN = new BufferedReader(f);
+
+			String line = fIN.readLine();
+
+			while (line != null) {
+				v.addElement(line);
+				line = fIN.readLine();
+			}
+
+			fIN.close();
+
+		} catch (Exception e) {
+			System.out.println("Errore: " + e);
+			try {
+				FileWriter f1 = new FileWriter("missioni.txt");
+				PrintWriter f1OUT = new PrintWriter(f1);
+				f1OUT.close();
+			} catch (Exception e1) {
+				System.out.println("Errore: " + e1);
+			}
+		}
+
+		return v;
+	}
+
+	public String paginaMissione(Vector<String> v, int scelta) {
+
+		// stampo dettagli missione
+
+		int i = 1;
+
+		try {
+			FileReader f = new FileReader("missioni.txt");
+			BufferedReader fIN = new BufferedReader(f);
+
+			String line = fIN.readLine();
+			String details = "";
+
+			while (line != null) {
+				if (i == scelta) {
+					details = line;
+					// fermo il ciclo
+					line = null;
+				} else {
+					line = fIN.readLine();
+					i++;
+				}
+			}
+
+			fIN.close();
+
+			return details;
+		} catch (Exception e) {
+			System.out.println("Errore: " + e);
+		}
+
+		return null;
+	}
+
+	public Vector<String> retPersonaggi() {
+		try {
+			FileReader f = new FileReader("personaggi_ottenuti.txt");
+			BufferedReader fIN = new BufferedReader(f);
+
+			Vector<String> v = new Vector<>();
+
+			String line = fIN.readLine();
+
+			while (line != null) {
+				v.addElement(line);
+
+				line = fIN.readLine();
+			}
+			fIN.close();
+
+			return v;
+		} catch (Exception e) {
+			System.out.println("Errore: " + e);
+		}
+
+		return null;
+	}
+
+	public static void main(String args[]) throws InterruptedException {
 		Scanner scanner = new Scanner(System.in);
 
 		// Variabili
@@ -155,7 +269,7 @@ public class ProjectRPG {
 				{ " |                                | " },
 				{ " |       ___________________      | " },
 				{ " |      |                   |     | " },
-				{ " |      |  ---> 1) Gioca    |     | " },
+				{ " |      |  1) Gioca         |     | " },
 				{ " |      |                   |     | " },
 				{ " |      |  2) Negozio       |     | " },
 				{ " |      |                   |     | " },
@@ -172,7 +286,7 @@ public class ProjectRPG {
 
 		clearScreen(); // Pulisce lo schermo
 
-		MenuPrincipale(menu, key, posY); // Funzione che stampa il menu principale
+		MenuPrincipale(menu); // Funzione che stampa il menu principale
 
 		scelta[0] = scanner.nextLine(); // Scegli una opzione
 
@@ -186,11 +300,12 @@ public class ProjectRPG {
 
 				// Se premi un tasto diverso da 1 torni al menu principale
 				if (scelta_negozio != 1) {
-					MenuPrincipale(menu, key, posY);
+					MenuPrincipale(menu);
 				}
 				// Altrimenti se premi 1 evochi un personaggio casuale
 				else {
 					// Evoca()
+					getRandomPlayer();
 				}
 				break;
 			case "3": // Visualizza titoli di coda
@@ -200,10 +315,5 @@ public class ProjectRPG {
 				System.out.println("Sei uscito dal gioco.");
 				break;
 		}
-	}
-
-	private static void MenuPrincipale(String[][] menu, String[] key, int posY) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'MenuPrincipale'");
 	}
 }
